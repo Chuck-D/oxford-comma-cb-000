07884b7d83488beea0d7eac0a5285a9c2cd2a248
Adrian Davis <chuck.d.kc@gmail.com>
2018-05-13 19:31:18 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,12 @@
 def oxford_comma(array)
   if array.length <2 
     my_str = array.join(", ")
+    
   elsif array.length == 2 
     my_str = array.join(' and ')
+    
+  elsif array.length >3
+    my_str = array.join(',').pop
+    
 end
 end
