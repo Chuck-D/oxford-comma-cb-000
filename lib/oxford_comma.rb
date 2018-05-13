f7b20cf83bd6dc0b145a24977c77c2cb76edd05e
Adrian Davis <chuck.d.kc@gmail.com>
2018-05-13 20:21:54 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,12 @@
 def oxford_comma(array)
+  #account for arrays of different sizes
   if array.length <2 
     my_str = array.join(", ")
     
   elsif array.length == 2 
     my_str = array.join(' and ')
     
+  #remove last element and insert later with and
   elsif array.length >=3
     x = array.pop
     my_str = array.join(', ')
