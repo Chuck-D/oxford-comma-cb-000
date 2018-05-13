f32c64f3b8d7b99752bb1dfcfacfda14ceb0a5d6
Adrian Davis <chuck.d.kc@gmail.com>
2018-05-13 20:16:08 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,6 +6,11 @@ def oxford_comma(array)
     my_str = array.join(' and ')
     
   elsif array.length >=3
+   x = array.pop
+  my_str = array.join(', ')
+  
+  my_str.insert(-1, ", and #{x}")
+  return my_str
     
     
 end
